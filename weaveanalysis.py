0b2a89d083df22e057a3aa466e1a21da46e7493b
namu-21 <namu00021@gmail.com>
2025-12-13 19:49:48 +0100

    Added error processing

--- weaveanalysis.py
+++ weaveanalysis.py
@@ -25,11 +25,10 @@ def bocao(arr, x1, y1, x2, y2):
 
     out = arr.copy()
 
-    # Ordenamos correctamente
+    # Ordenado porque los tienen los pixeles al reves
     xmin, xmax = sorted([x1, x2])
     ymin, ymax = sorted([y1, y2])
 
-    # Convertimos a (fila=y, columna=x)
     out[ymin:ymax+1, xmin:xmax+1] = 0
 
     return out
@@ -54,23 +53,21 @@ def segmentation_map(image, brightness_ranges):
     """
 
     if len(brightness_ranges) != 10:
-        raise ValueError("Debes proporcionar exactamente 10 rangos (10 máscaras).")
+        raise ValueError("No se han dado diez rangos")
 
-    # Inicialmente, ningún píxel pertenece a nada
-    seg = np.zeros_like(image, dtype=np.uint8)
+    segmap = np.zeros_like(image, dtype=np.uint8)
 
     for i, (Bmin, Bmax) in enumerate(brightness_ranges):
         mask = (image >= Bmin) & (image <= Bmax)
 
-        # Etiquetamos la máscara con valor i+1 (1..10)
-        seg[mask] = i + 1
+        segmap[mask] = i + 1
 
-    return seg
+    return segmap
 
 
-def sum_spectra(cube, segmap, n_masks=10):
+def avg_spectra(cube, segmap, n_masks=10, do_sum = False):
     """
-    Suma los espectros contenidos en cada máscara para cubos con formato (Nλ, Nx, Ny).
+    Media los espectros contenidos en cada máscara para cubos con formato (Nλ, Nx, Ny).
 
     Parameters
     ----------
@@ -101,19 +98,17 @@ def sum_spectra(cube, segmap, n_masks=10):
             spectra[i] = np.zeros(Nlambda)
             continue
 
-        # Extraemos los píxeles pertenecientes a la máscara para cada λ
-        # cube[:, mask] => matriz de shape (Nlambda, N_pixels_mask)
         masked_cube = cube[:, mask]
-
-        # Sumamos sobre los píxeles
-        spectra[i] = masked_cube.sum(axis=1)
+        if do_sum:
+            spectra[i] = masked_cube.sum(axis=1)
+        else:
+            spectra[i] = masked_cube.mean(axis=1)
 
     return spectra
 
 if __name__ == "__main__":
 
     from astropy.io import fits
-    from astropy.visualization import simple_norm
     import matplotlib.pyplot as plt
     import numpy as np
     
@@ -121,14 +116,16 @@ if __name__ == "__main__":
     fileblue ="WEAVE/stackcube_3120048.fit"
 
     red = fits.open(filered)
-    rd = red[1].data
+    rdata = red[1].data
     rflux = red[6].data
+    rerr = red[2].data
     
     red.close()
     
     blue = fits.open(fileblue)
-    bd = blue[1].data
+    bdata = blue[1].data
     bflux = blue[6].data
+    berr = blue[2].data
     
     blue.close()
     
@@ -139,6 +136,8 @@ if __name__ == "__main__":
     cflux = bocao(flux, 27, 35, 60, 15)
     cflux = bocao(cflux, 54, 173, 64, 165)
     cflux = bocao(cflux, 152, 127, 163, 117)
+    
+    #Seleccion de intervalos de brillo
     
     bright_tuples =[
         (9e6, 1e9),
@@ -153,15 +152,32 @@ if __name__ == "__main__":
         (1e5,2e5)
         ]
     
-    seg = segmentation_map(cflux, bright_tuples)
-    red_spectra = sum_spectra(rd, seg)
-    blue_spectra = sum_spectra(bd, seg)
+    segmap = segmentation_map(cflux, bright_tuples)
+    red_spectra = avg_spectra(rdata, segmap)
+    blue_spectra = avg_spectra(bdata, segmap)
+
+    red_err = avg_spectra(rerr, segmap, do_sum = False)
+    blue_err = avg_spectra(berr, segmap, do_sum = False)
+    #%%
+    red_correction = np.ones(11)
+    for i in range(1,10):
+        med = np.nanmedian((red_spectra[i][1:]-red_spectra[i][:-1])**2)
+        mu = np.nanmedian(red_err[i])
+        red_correction[i] = med/mu
+        print(f'For mask {i}, med = {med}, mu = {mu}, corr = {red_correction[i]}')
+    
+    blue_correction = np.ones(11)
+    for i in range(1,10):
+        med = np.nanmedian((blue_spectra[i][1:]-blue_spectra[i][:-1])**2)
+        mu = np.nanmedian(blue_err[i])
+        blue_correction[i] = med/mu
+        print(f'For mask {i}, med = {med}, mu = {mu}, corr = {blue_correction[i]}')
     
     Rrange = (5790, 9590)
     Brange = (3660, 6060)
     
-    Rwav = np.arange(Rrange[0], Rrange[1], (Rrange[1] - Rrange[0]) / len(rd))
-    Bwav = np.arange(Brange[0], Brange[1], (Brange[1] - Brange[0]) / len(bd))
+    Rwav = np.arange(Rrange[0], Rrange[1], (Rrange[1] - Rrange[0]) / len(rdata))
+    Bwav = np.arange(Brange[0], Brange[1], (Brange[1] - Brange[0]) / len(bdata))
     
     plt.figure(1)
     flux_show(cflux)
@@ -169,13 +185,21 @@ if __name__ == "__main__":
     mask_plot = True
     plt.figure(2)
     if mask_plot:
-        plt.imshow(np.transpose(seg), origin='lower', cmap='tab10')
+        plt.imshow(np.transpose(segmap), origin='lower', cmap='tab10')
         plt.show()
             
     mask2plot = 2
     plt.figure(3)
-    plt.plot(Rwav, red_spectra[mask2plot],Bwav, blue_spectra[mask2plot])
+    plt.plot(Rwav, red_spectra[mask2plot],'k')
+    plt.plot(Rwav, red_spectra[mask2plot]+red_err[mask2plot]*red_correction[mask2plot],'k--', alpha = 0.3)
+    plt.plot(Rwav, red_spectra[mask2plot]-red_err[mask2plot]*red_correction[mask2plot],'k--', alpha = 0.3)
+    plt.plot(Bwav, blue_spectra[mask2plot],'k')
+    plt.plot(Bwav, blue_spectra[mask2plot]+blue_err[mask2plot]*blue_correction[mask2plot],'k--', alpha = 0.3)
+    plt.plot(Bwav, blue_spectra[mask2plot]-blue_err[mask2plot]*blue_correction[mask2plot],'k--', alpha = 0.3)
     plt.xlabel('Wavelength (Armstrong)')
     plt.ylabel('Intensity (au)')
     plt.grid(True)
+    plt.title(f'Spectra of mask {mask2plot}')
+    
+    
     
